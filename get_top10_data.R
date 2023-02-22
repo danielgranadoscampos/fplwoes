758b2387973348e55937d8e1b1ee2788fc20711f
danielgranadoscampos <granadoscampos.daniel@gmail.com>
2023-02-21 23:22:58 -0600

    Function to get ten top player worldwide

---
+++ get_top10_data.R
@@ -0,0 +1,21 @@
+#library(fplscrapR)
+get_top10_data <- function (){
+  
+  top_entries <- get_league_entries(leagueid = 314) |>
+    arrange(desc(rank)) |> head(10)
+  
+  top_10_ids <- top_entries$entry
+  
+  top_season_info <- map_dfr(top_10_ids, get_entry_season) |> 
+    tibble() #|> select(total_points, event) |>  tibble()
+  
+  average_top10_scores <- top_season_info |>
+    group_by(event) |>
+    summarise(total_points = mean(total_points)) |> #name changed
+    mutate(ind = "avg_top_10") 
+  
+  return(average_top10_scores)
+}
+
+
+
